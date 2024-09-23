74fd56dffe5356364b5003436f09f2fe872dfb3d
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 09:21:37 +0000

    4.17.4

---
+++ revolut-gateway-for-woocommerce/client/dist/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '71ffd96ee2a5bf0a463d');
---
+++ revolut-gateway-for-woocommerce/client/hooks/index.js
@@ -0,0 +1,3 @@
+export { useCardField } from './use-card-field'
+export { usePaymentRequest } from './use-payment-request'
+export { useHidePlacerOrderButton } from './use-hide-place-order-button'
