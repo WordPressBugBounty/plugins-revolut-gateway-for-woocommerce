0a99228305cb8bc7165997191682845e0d0bb545
Sean Murphy <sean@iamseanmurphy.com>
2025-07-17 09:19:51 +0000

    4.20.1

--- revolut-gateway-for-woocommerce/client/dist/client/blocks.index.asset.php
+++ revolut-gateway-for-woocommerce/client/dist/client/blocks.index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b2f3a1a95236a05c10bb');
+<?php return array('dependencies' => array('react', 'wc-blocks-checkout', 'wc-blocks-registry', 'wc-settings', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '608930f9e7f4d393938a');
--- revolut-gateway-for-woocommerce/vendor/composer/installed.php
+++ revolut-gateway-for-woocommerce/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'woocommerce/revolut-gateway-for-woocommerce',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '08b88ab5f7b8da341efe96aba619fb050352aab0',
+        'reference' => '8156b0a240ea557b3b824e8c0c4c25a0c41b267c',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -22,7 +22,7 @@
         'woocommerce/revolut-gateway-for-woocommerce' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '08b88ab5f7b8da341efe96aba619fb050352aab0',
+            'reference' => '8156b0a240ea557b3b824e8c0c4c25a0c41b267c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
